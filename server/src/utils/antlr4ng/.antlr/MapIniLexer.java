3d43343918fe23e63cd8944a06c0406d0ace15c4
ViTeXFTW <mads213@gmail.com>
2024-10-25 17:38:56 +0200

    Fixed: #31 KindOf now has ability to add/remove kindofs with +/-

--- server/src/utils/antlr4ng/.antlr/MapIniLexer.java
+++ server/src/utils/antlr4ng/.antlr/MapIniLexer.java
@@ -1,4 +1,4 @@
-// Generated from c:/Users/Mads/Sync/ZeroSyntax/ZeroSyntax-Server/server/src/utils/antlr4ng/MapIni.g4 by ANTLR 4.13.1
+// Generated from c:/Users/Mads/Sync/ZeroSyntax-Server/server/src/utils/antlr4ng/MapIni.g4 by ANTLR 4.13.1
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Token;
